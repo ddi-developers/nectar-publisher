048fbc29f877a888193dd7059814ce9bd6d34930
Olof Olsson <olof.olsson@snd.gu.se>
2023-10-22 22:22:30 +0200

    some random tests

--- resources/modules/utils.js
+++ resources/modules/utils.js
@@ -200,6 +200,19 @@ async function checksum(file, type){
   return hashHex;
 }
 
+function median_of_arr(arr) { 
+  const middle = (arr.length + 1) / 2; 
+
+  const sorted = [...arr].sort((a, b) => a - b); 
+  const isEven = sorted.length % 2 === 0; 
+
+  return isEven ? (sorted[middle - 1.5] 
+      + sorted[middle - 0.5]) / 2 : 
+      sorted[middle - 1]; 
+} 
+var arr = [1, 4, 7, 9]; 
+console.log(median_of_arr(arr)); 
+
 function getAppMetadata(){
   return JSON.parse(document.head.querySelector('script[type="application/ld+json"]').innerText);
 }
