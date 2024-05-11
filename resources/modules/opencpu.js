305a88ace9ffd2f58af595d52244b00dfb433294
JulieLen16 <julie.lenoir@ined.fr>
2024-05-11 12:46:01 +0200

    Add retrieving csv from spss file

--- resources/modules/opencpu.js
+++ resources/modules/opencpu.js
@@ -9,6 +9,11 @@ curl https://cloud.opencpu.org/ocpu/library/haven/R/read_sav -F "file=@well_bein
 // Getting attributes for each variable
 curl https://cloud.opencpu.org/ocpu/library/base/R/lapply -d "X=IDREP&FUN=function(x) attributes(x)"
 
+// Getting csv from file
+curl https://cloud.opencpu.org/ocpu/library/utils/R/write.csv -d "x=x0b8bd45a2e949b&file='test.csv'"
+curl https://cloud.opencpu.org/ocpu/tmp/x0b17a01642b5cd/files/test.csv    
+
+
 > GET ID for directory (IDREP2)
 
 // Getting codeLists
@@ -20,6 +25,8 @@ curl https://cloud.opencpu.org/ocpu/library/base/R/lapply -d "X=IDREP&FUN=functi
 curl https://cloud.opencpu.org/ocpu/tmp/IDREP2/R/.val/json
 curl https://cloud.opencpu.org/ocpu/tmp/IDREP3/R/.val/json
  
+
+
  */
 class OpenCPU{
 
@@ -52,11 +59,50 @@ class OpenCPU{
             return response.text();
         })
         .then(responseText => { 
+
             console.log("RESULT FROM: " + base + "library/haven/R/read_sav")
             var id1 = getId(responseText)
 
             console.log("ID1: " +id1)
             
+            // Getting CSV from file
+            //curl https://cloud.opencpu.org/ocpu/library/utils/R/write.csv -d "x=x0b8bd45a2e949b&file='test.csv'"
+            //curl https://cloud.opencpu.org/ocpu/tmp/x0b17a01642b5cd/files/test.csv    
+
+            var data = new FormData()
+            data.append('x', id1)
+            data.append('file', "'data.csv'")
+
+            fetch(base + "library/utils/R/write.csv", {
+                method: 'POST',
+                body: data
+            })
+            .then(response => {
+                // Check if the response is successful
+                if (!response.ok) {
+                  throw new Error('Network response was not ok for writeCSV');
+                }
+                // Read the response content as text
+                return response.text();
+            })
+            .then(responseText => {
+                var id4 = getId(responseText)
+                console.log("ID4: " + id4)
+
+                fetch(base +`/tmp/${id4}/files/data.csv`)
+                .then(response => {
+                    // Check if the response is successful (HTTP status code 200)
+                    if (!response.ok) {
+                      throw new Error('Network response was not ok for getCSV');
+                    }
+                    // Parse the response body as text
+                    return response.text();
+                })
+                .then(csvContent => {
+                    dataset.
+                })
+            })
+
             // Getting attributes for each variable
             // curl https://cloud.opencpu.org/ocpu/library/base/R/lapply -d "X=IDREP&FUN=function(x) attributes(x)"
             
@@ -90,7 +136,6 @@ class OpenCPU{
                     return response.json();
                 })
                 .then(json => {
-    
 
                     // Getting codeLists
                     //curl https://cloud.opencpu.org/ocpu/library/base/R/lapply -d "X=IDREP&FUN=function(x) as.list(attributes(x)$labels)"
