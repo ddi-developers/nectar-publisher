398712c5efeec39c6f5b5d1a7a26bf72cd7c54eb
Olof Olsson <olof.olsson@snd.gu.se>
2025-08-19 17:02:42 +0200

    wip: refactoring stuff
    need to continue refactoring...

--- resources/modules/importers/ddi-c-xml-importer.js
+++ resources/modules/importers/ddi-c-xml-importer.js
@@ -20,35 +20,29 @@ function readDDiCString(ddiCString){
     var parser = new DOMParser();
     var doc = parser.parseFromString(ddiCString, "application/xml");
 
-    for (const child of doc.documentElement.children) {
-        if(child.nodeName === 'dataDscr') {
-            var gChildren = child.children;
-            for (var i = 0; i < gChildren.length; i++) {
-                if (gChildren[i].nodeName === 'var'){
-                    var gChild = gChildren[i];
-                    var var_labl ='';
-                    var var_dscr ='';
+    var vars = doc.getElementsByTagName('var');
+    
+    for (var i = 0; i < vars.length; i++) {
+        var gChild = vars[i];
+        var var_labl ='';
+        var var_dscr ='';
 
-                    var ggChildren = gChild.children;
-                    for (var j = 0; j < ggChildren.length; j++) {
-                        if (ggChildren[j].nodeName === 'labl'){
-                            var_labl = ggChildren[j].textContent.trim();
-                        } else if (ggChildren[j].nodeName === 'txt'){
-                            var_dscr = ggChildren[j].textContent.trim();
-                        }
-                    }
-                    var_dictionary[gChild.getAttribute("name").trim()] = [
-                        var_labl, 
-                        var_dscr, 
-                        gChild.getAttribute("representationType")
-                    ]
-                }
+        var ggChildren = gChild.children;
+        for (var j = 0; j < ggChildren.length; j++) {
+            if (ggChildren[j].nodeName === 'labl'){
+                var_labl = ggChildren[j].textContent.trim();
+            } else if (ggChildren[j].nodeName === 'txt'){
+                var_dscr = ggChildren[j].textContent.trim();
             }
         }
+        var_dictionary[gChild.getAttribute("name").trim()] = [
+            var_labl, 
+            var_dscr, 
+            gChild.getAttribute("representationType")
+        ]
     }
 
     return var_dictionary;
-
 }
 
 export {
